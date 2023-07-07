0797746c98a714447ca296fa7a5de242bf50192d
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-07-07 18:38:17 -0300

    css and first components add

--- drum machine/src/App.jsx
+++ drum machine/src/App.jsx
@@ -6,7 +6,13 @@ function App() {
 
   return (
     <>
-      
+      <div className='inner-container' id="drum-machine">
+        <div className='pad-bank'></div>
+        <div id="logo">
+          <div className='inner-logo'>FCC</div>
+          <i className='inner-logo fa fa-fire'></i>
+        </div>
+      </div>
     </>
   )
 }
