5f8c95fe296b9c3e45c6fe2fc4dae2c4baefeca6
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-07-10 06:34:42 -0300

    initialized Controls component
    element with id="display" add

--- drum machine/src/App.jsx
+++ drum machine/src/App.jsx
@@ -1,6 +1,7 @@
 import {  } from 'react'
 import './App.css'
 import PadBank from './components/PadBank'
+import Controls from './components/Controls'
 
 function App() {
 
@@ -8,11 +9,12 @@ function App() {
   return (
     <>
       <div className='inner-container' id="drum-machine">
-      <PadBank />
+        <PadBank />
         <div id="logo">
           <div className='inner-logo'>FCC</div>
           <i className='inner-logo'></i>
         </div>
+        <Controls />
       </div>
     </>
   )
---
+++ drum machine/src/components/Controls.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Controls = () => {
+  return (
+    <>
+    <div className="controls-container">
+      <div id="display">Hello</div>
+    </div>
+    </>
+  )
+}
+
+export default Controls;
