2b7ed7a63a51a6abe827d8cefd0c9a0303da22e8
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-07-11 16:00:28 -0300

    add css change for drumpads and onchange for volumeslider

--- drum machine/src/components/VolumeSlider.jsx
+++ drum machine/src/components/VolumeSlider.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const VolumeSlider = () => {
+  const [volume, setVolume] = useState(0.5);
+
+  const handleVolumeChange = (event) => {
+    const newVolume = parseFloat(event.target.value);
+    setVolume(newVolume);
+  };
+
   return (
     <>
       <div className="volume-slider">
-        <input max={1} min={0} step={0.01} type="range" value={0.5}></input>
+        <input
+          max={1}
+          min={0}
+          step={0.01}
+          type="range"
+          value={volume}
+          onChange={handleVolumeChange}
+        />
       </div>
     </>
-  )
-}
+  );
+};
 
 export default VolumeSlider;
